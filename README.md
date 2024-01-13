c4e26814e043a2a339a6d72b1ac3ba0a41f562ee
Vivek Chaugule <49585527+vivekkchaugule@users.noreply.github.com>
2024-01-13 05:45:30 +0000

    Practiced Building Blocks

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# LangChain
+# Building Blocks
+
+<head>Language Model</head>
+<ol>
+    <li> Text LLMs: Fn (string) -> string
+    <li> Chat models: list of messages -> single message output
+</ol>
+
+<head> Prompt Template
+<ol>
+    <li> We can create Prompt Template using <b>ChatPromptTemplate.</b>
+    <li> LangChain Expression Langauge (LCEL)
+    <li> Output Parser - Convert Response JSON to String.
+    <li> Runnable Sequence - Create sequence to be executed.
+    <li> Streaming - Get the response few characters at a time.
+    <li> Batch - Run model against batch of inputs at a time.
+</ol>
+
--- index.js
+++ index.js
@@ -1,19 +1,126 @@
-import { OpenAI } from "@langchain/openai";
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage } from "@langchain/core/messages";
 
-// Define LLM Model
-const model = new OpenAI({
-    modelName: "gpt-3.5-turbo-instruct", // Defaults to "gpt-3.5-turbo-instruct" if no model provided.
-    temperature: 0.9,
+// Define ChatModel
+const model = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo-1106",
     openAIApiKey: process.env.OPEN_API_KEY
 });
 
-// Invoke Model
-const res = await model.invoke(
-    "What would be a good company name a company that makes colorful underwares?"
-);
+// Part 1: Direct Invocation
 
-// Print Response
-console.log(res);
+const res = await model.invoke([
+    new HumanMessage("Who has branches but its not tree?")
+]);
+
+console.log(JSON.stringify(res));
+
+// Part 2: Prompt Based Invocation
+
+import {ChatPromptTemplate} from '@langchain/core/prompts';
+
+const prompt = ChatPromptTemplate.fromTemplate(`
+    What are four good names for a company that produce {product} ?
+`);
+
+const formattedPrompt = await prompt.format({
+    product: "Fancy Lamps"
+});
+
+console.log(formattedPrompt);
+
+const formattedMessages = await prompt.formatMessages({
+    product: "Fancy Lamps"
+});
+
+console.log(formattedMessages);
+
+// Part 3: Adding SystemTemplate
+
+import {
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate
+} from '@langchain/core/prompts';
 
 
+const promptFromMessages = ChatPromptTemplate.fromMessages([
+    SystemMessagePromptTemplate.fromTemplate("You are an {level} at picking company names."),
+    HumanMessagePromptTemplate.fromTemplate("What are two good names for a company that makes {prod}")
+]);
 
+const formattedPromptFromMessages = await promptFromMessages.formatMessages({
+    level: "beginner",
+    prod: "shiny diamonds"
+});
+
+console.log(formattedPromptFromMessages);
+
+// Part 4: Other ways to do the same
+
+const promptFromMessages_2 = ChatPromptTemplate.fromMessages([
+    ["system", "You are an {level} at playing BlackJack!"],
+    ["human", "What are three good names for a company that produces {prod}?"]
+]);
+
+const promptFromMessages_2_formatted = await promptFromMessages_2.formatMessages({
+    level: "expert",
+    prod: "Shampoo"
+});
+
+console.log(promptFromMessages_2_formatted);
+
+
+// Part 5: LangChain Expression Language (LCEL)
+const chain = prompt.pipe(model);
+
+const result = await chain.invoke({
+    product: "colourful socks"
+});
+
+console.log(result);
+
+
+// Part 6: OutPut Parser - Convert response JSON to String 
+
+import {StringOutputParser} from "@langchain/core/output_parsers";
+
+const outputParser = new StringOutputParser();
+const nameGenerationChain = prompt.pipe(model).pipe(outputParser);
+
+const stringResult = await nameGenerationChain.invoke({
+    product: "Fancy Lipstick"
+});
+
+console.log(stringResult);
+
+// Part 7: Runnable Squence - Merge Model, Prompt, OutputParser into one
+
+import {RunnableSequence} from "@langchain/core/runnables";
+
+const runnableSeq = RunnableSequence.from([
+    prompt,
+    model,
+    outputParser
+]);
+
+console.log(await runnableSeq.invoke({ product: "Memory Card" }));
+
+// Part 8: Streaming
+const stream = await runnableSeq.stream({
+    product: "Avocado Cookies"
+});
+
+for await(const chunk of stream) {
+    console.log(chunk);
+}
+
+// Part 9 : Batch
+
+const inputs = [
+    {product: "Fancy cookies"},
+    {product: "Crispy Donut"}
+];
+
+const result_batch = await runnableSeq.batch(inputs);
+
+console.log(result_batch);
